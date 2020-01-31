b3d5a75c9bab5d5ccfb8db6b62362d7f25acf693
UcheSylvester <okorocode@gmail.com>
2020-01-31 16:32:05 +0100

    built out home page structure

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -13,43 +13,31 @@ const HomePage = () => (
 
       <div className="movie">
         <div className="content">
-          <h1 className="title">FLASH</h1>
+          <h1 className="title"> LUCIFER</h1>
         </div>
       </div>
 
       <div className="movie">
         <div className="content">
-          <h1 className="title">FLASH</h1>
+          <h1 className="title">DECEPTION</h1>
         </div>
       </div>
 
       <div className="movie">
         <div className="content">
-          <h1 className="title">FLASH</h1>
+          <h1 className="title">MERLIN</h1>
         </div>
       </div>
 
       <div className="movie">
         <div className="content">
-          <h1 className="title">FLASH</h1>
+          <h1 className="title">LEGEND OF THE SEEKER</h1>
         </div>
       </div>
 
       <div className="movie">
         <div className="content">
-          <h1 className="title">FLASH</h1>
-        </div>
-      </div>
-
-      <div className="movie">
-        <div className="content">
-          <h1 className="title">FLASH</h1>
-        </div>
-      </div>
-
-      <div className="movie">
-        <div className="content">
-          <h1 className="title">FLASH</h1>
+          <h1 className="title">REDEMPTION</h1>
         </div>
       </div>
     </div>
