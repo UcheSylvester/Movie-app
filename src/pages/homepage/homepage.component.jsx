ce6e3cbf1a77d36b8d7df719dbf67e325a670777
UcheSylvester <okorocode@gmail.com>
2020-01-31 16:13:57 +0100

    building out home page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="movies-directory">
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+
+      <div className="movie">
+        <div className="content">
+          <h1 className="title">FLASH</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
